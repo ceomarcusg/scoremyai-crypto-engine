4eb6e178e4e7b58062a485c27426935f64e1b4e3
ceomarcusg <marcusglenn@mdotglobal.com>
2026-03-10 09:10:10 +0600

    Update crypto_score.py

---
+++ scoring/crypto_score.py
@@ -0,0 +1,16 @@
+def calculate_crypto_score(data):
+
+    score = 0
+
+    if data["market_cap"] > 100000000000:
+        score += 30
+
+    if data["volume"] > 1000000000:
+        score += 20
+
+    if data["github_repos"] > 100:
+        score += 20
+
+    score += 30  # baseline network strength
+
+    return min(score, 100)
