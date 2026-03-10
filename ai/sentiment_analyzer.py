6f2ef7778b420f72c452473a993b0ea69037bd88
ceomarcusg <marcusglenn@mdotglobal.com>
2026-03-10 09:10:41 +0600

    Update sentiment_analyzer.py

---
+++ ai/sentiment_analyzer.py
@@ -0,0 +1,11 @@
+from textblob import TextBlob
+
+def analyze_sentiment(text):
+    sentiment = TextBlob(text).sentiment.polarity
+
+    if sentiment > 0.2:
+        return "Bullish"
+    elif sentiment < -0.2:
+        return "Bearish"
+    else:
+        return "Neutral"
