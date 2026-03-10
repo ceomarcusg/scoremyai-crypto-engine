776fb222d63610cf8a406969c2e0ad0da861f7bd
ceomarcusg <marcusglenn@mdotglobal.com>
2026-03-10 09:31:00 +0600

    market_data.py

--- data/market_data.py
+++ data/market_data.py
@@ -1,1 +1,17 @@
+import requests
 
+def get_crypto_data(coin="bitcoin"):
+
+    url = f"https://api.coingecko.com/api/v3/coins/{coin}"
+
+    response = requests.get(url)
+    data = response.json()
+
+    market_data = {
+        "price": data["market_data"]["current_price"]["usd"],
+        "market_cap": data["market_data"]["market_cap"]["usd"],
+        "volume": data["market_data"]["total_volume"]["usd"],
+        "github_repos": data["developer_data"]["forks"],
+    }
+
+    return market_data
