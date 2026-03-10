1853139539198c4a516fd4f853332d7904e52a03
ceomarcusg <marcusglenn@mdotglobal.com>
2026-03-10 09:09:35 +0600

    Update server.py

---
+++ api/server.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from data.market_data import get_crypto_data
+from scoring.crypto_score import calculate_crypto_score
+
+app = FastAPI()
+
+@app.get("/score/{coin}")
+
+def score_crypto(coin: str):
+
+    data = get_crypto_data(coin)
+
+    score = calculate_crypto_score(data)
+
+    return {
+        "coin": coin,
+        "price": data["price"],
+        "scoremyai_rating": score
+    }
